5b8f0a4b45655ce2d2fd006caa7914334a490157
Gyanendra Singh <gyanendra@blohash.com>
2022-12-20 00:13:22 +0530

    dev

--- models/users.ts
+++ models/users.ts
@@ -2,10 +2,13 @@ import { Schema, model, ObjectId } from "mongoose";
 
 interface User {
   name: string;
-  last_name:string,
-  password:string,
-  wallet_data_bep2:string,
-  user_type:string,
+  last_name: string;
+  password: string;
+  wallet_data_bep2: string;
+  user_type: string;
+  dob: string;
+  confirm_correctness: boolean;
+  telegram: string;
   email: string;
   no_of_nfts_owned: number;
   nfts_token_id: Array<string>;
@@ -16,18 +19,20 @@ interface User {
 }
 const schema = new Schema<User>(
   {
-    name: { type: String, required: false},
-    last_name: { type: String, required: false},
-    password: { type: String, required: false},
-    wallet_data_bep2: { type: String, required: false},
-    user_type: { type: String, required: false},
-    email: {type: String,required: false},
-    no_of_nfts_owned: {type: Number,default:0,  required: false},
-    nfts_token_id: [{type: String,required: false}],
-    wallet: {type: String,required: false},
-    winnings: {type: Number,default:0,required: false},
-    lossings: {type: Number,default:0,required: false},
-    active: {type: Boolean,required: false,default: true},
+    name: { type: String, required: false },
+    last_name: { type: String, required: false },
+    password: { type: String, required: false },
+    wallet_data_bep2: { type: String, required: false },
+    user_type: { type: String, required: false },
+    dob: { type: String, required: false },
+    confirm_correctness: { type: Boolean, required: false },
+    email: { type: String, required: false },
+    no_of_nfts_owned: { type: Number, default: 0, required: false },
+    nfts_token_id: [{ type: String, required: false }],
+    wallet: { type: String, required: false },
+    winnings: { type: Number, default: 0, required: false },
+    lossings: { type: Number, default: 0, required: false },
+    active: { type: Boolean, required: false, default: true },
   },
   {
     timestamps: true,
